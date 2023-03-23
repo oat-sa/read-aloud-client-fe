83630a52c114b023d3f1852f7253633e4bd4b556
jsconan <jsconan@gmail.com>
2023-03-23 11:46:58 +0100

    chore: replace rollup-plugin-terser by @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -18,16 +18,18 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
-export default [{
-    input: 'index.js',
-    plugins: [resolve(), commonjs(), babel({ babelHelpers: 'bundled' }), terser()],
-    output: {
-        file: 'dist/index.js',
-        format: 'iife',
-        name: 'readAloudClient',
-        exports: 'named',
-        sourcemap: true
+export default [
+    {
+        input: 'index.js',
+        plugins: [resolve(), commonjs(), babel({ babelHelpers: 'bundled' }), terser()],
+        output: {
+            file: 'dist/index.js',
+            format: 'iife',
+            name: 'readAloudClient',
+            exports: 'named',
+            sourcemap: true
+        }
     }
-}];
+];
