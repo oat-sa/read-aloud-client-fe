88e06f85967591c7b9fc90ca32b4c2bcc62febb2
mike-ionut-mihai-sandu-tao <ionut-mihai@taotesting.com>
2026-07-28 12:18:29 +0200

    chore(license): switch SPDX headers to IDE block-comment template
    Use /** */ copyright blocks so IDEs can manage SPDX-FileCopyrightText year ranges, with Copyright (C) set to the current year only.

--- index.amd.js
+++ index.amd.js
@@ -1,7 +1,8 @@
-// SPDX-FileCopyrightText: 2012-2026 Open Assessment Technologies S.A.
-// Copyright (C) 2026 (original work) Open Assessment Technologies S.A.
-//
-// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-TAO-Commercial-License
-
+/**
+ * SPDX-FileCopyrightText: 2012-2026 Open Assessment Technologies S.A.
+ * Copyright (C) 2026 (original work) Open Assessment Technologies S.A.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-TAO-Commercial-License
+ */
 
 export { getAvailableProviders, default as getReadAloudClient } from './index.js';
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
-// SPDX-FileCopyrightText: 2012-2026 Open Assessment Technologies S.A.
-// Copyright (C) 2026 (original work) Open Assessment Technologies S.A.
-//
-// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-TAO-Commercial-License
+/**
+ * SPDX-FileCopyrightText: 2012-2026 Open Assessment Technologies S.A.
+ * Copyright (C) 2026 (original work) Open Assessment Technologies S.A.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-TAO-Commercial-License
+ */
 
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
